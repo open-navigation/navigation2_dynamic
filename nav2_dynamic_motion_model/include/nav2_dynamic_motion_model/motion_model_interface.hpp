2f1e051edd8f4270650aada18810c53f082b181e
Chiragkumar Makwana <chiragmakwana02@gmail.com>
2025-02-11 21:05:15 +0530

    refactor
    Signed-off-by: Chiragkumar Makwana <chiragmakwana02@gmail.com>

--- nav2_dynamic_motion_model/include/nav2_dynamic_motion_model/motion_model_interface.hpp
+++ nav2_dynamic_motion_model/include/nav2_dynamic_motion_model/motion_model_interface.hpp
@@ -1,6 +1,7 @@
 #ifndef NAV2_DYNAMIC_MOTION_MODEL__MOTION_MODEL_INTERFACE_HPP_
 #define NAV2_DYNAMIC_MOTION_MODEL__MOTION_MODEL_INTERFACE_HPP_
 
+#include <memory>
 #include <geometry_msgs/msg/pose.hpp>
 #include <geometry_msgs/msg/pose_array.hpp>
 #include <geometry_msgs/msg/twist.hpp>
@@ -13,16 +14,21 @@ namespace nav2_dynamic_motion_model
 class MotionModelInterface
 {
 public:
+    using Ptr = std::shared_ptr<MotionModelInterface>;
+    using ConstPtr = std::shared_ptr<const MotionModelInterface>;
+
     virtual ~MotionModelInterface() = default;
 
     virtual geometry_msgs::msg::Pose predictObstaclePose(
         const nav2_dynamic_msgs::msg::Obstacle &obstacle,
         double dt) const = 0;
 
+
     virtual geometry_msgs::msg::PoseArray predictObstaclePoseArray(
         const nav2_dynamic_msgs::msg::ObstacleArray &obstacle_array,
         double dt) const = 0;
 };
 
-} //namespace
-#endif //NAV2_DYNAMIC_MOTION_MODEL__MOTION_MODEL_INTERFACE_HPP_
+} // namespace nav2_dynamic_motion_model
+
+#endif // NAV2_DYNAMIC_MOTION_MODEL__MOTION_MODEL_INTERFACE_HPP_
