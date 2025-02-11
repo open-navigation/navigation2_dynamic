fc61c9cbb3b0c6ef1ab3c66304ed824650337ab7
Chiragkumar Makwana <chiragmakwana02@gmail.com>
2025-02-11 21:10:29 +0530

    use ObstacleArray and MotionModelInterface ptr args
    Signed-off-by: Chiragkumar Makwana <chiragmakwana02@gmail.com>

--- nav2_dynamic_util/include/nav2_dynamic_util/tracked_obstacle_utils.hpp
+++ nav2_dynamic_util/include/nav2_dynamic_util/tracked_obstacle_utils.hpp
@@ -4,10 +4,10 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/header.hpp>
 #include <geometry_msgs/msg/pose.hpp>
-
+#include "geometry_msgs/msg/pose_stamped.hpp"
 #include <nav2_dynamic_msgs/msg/obstacle.hpp>
 #include <nav2_dynamic_msgs/msg/obstacle_array.hpp>
-#include "nav2_dynamic_motion_model/motion_model_base.hpp"
+#include "nav2_dynamic_motion_model/motion_model_interface.hpp"
 
 #include <random>
 #include <vector>
@@ -15,10 +15,15 @@
 
 namespace nav2_dynamic_util
 {
-    geometry_msgs::msg::Pose getObstaclePoseAt(const double dt, 
-                            const nav2_dynamic_msgs::msg::Obstacle obstacle, 
-                            const std::shared_ptr<nav2_dynamic_motion_model::MotionModelInterface> &motion_model);
+    geometry_msgs::msg::Pose getObstaclePoseAt(
+        double dt, 
+        const nav2_dynamic_msgs::msg::Obstacle &obstacle, 
+        const std::shared_ptr<nav2_dynamic_motion_model::MotionModelInterface> &motion_model);
 
+    geometry_msgs::msg::PoseArray getObstaclePoseArrayAt(
+        double dt, 
+        const nav2_dynamic_msgs::msg::ObstacleArray &obstacle_array, 
+        const std::shared_ptr<nav2_dynamic_motion_model::MotionModelInterface> &motion_model);
 
 } // namespace
 #endif  // NAV2_DYNAMIC_UTIL__TRACKED_OBSTACLE_UTILS_HPP_
--- nav2_dynamic_util/src/tracked_obstacle_utils.cpp
+++ nav2_dynamic_util/src/tracked_obstacle_utils.cpp
@@ -3,13 +3,18 @@
 namespace nav2_dynamic_util
 {
 
-    geometry_msgs::msg::Pose getObstaclePoseAt(const double dt, 
-                                const nav2_dynamic_msgs::msg::Obstacle obstacle, 
-                                const std::shared_ptr<nav2_dynamic_motion_model::MotionModelInterface> &motion_model)
-    {
-        
-        geometry_msgs::msg::Pose predicted_pose = motion_model->predictObstaclePose(obstacle, dt);
-        return predicted_pose;
-
+    geometry_msgs::msg::Pose getObstaclePoseAt(
+        double dt, 
+        const nav2_dynamic_msgs::msg::Obstacle &obstacle, 
+        const std::shared_ptr<nav2_dynamic_motion_model::MotionModelInterface> &motion_model){
+        return motion_model->predictObstaclePose(obstacle, dt);
     }
-}
+
+    geometry_msgs::msg::PoseArray getObstaclePoseArrayAt(
+        double dt, 
+        const nav2_dynamic_msgs::msg::ObstacleArray &obstacle_array, 
+        const std::shared_ptr<nav2_dynamic_motion_model::MotionModelInterface> &motion_model){
+        return motion_model->predictObstaclePoseArray(obstacle_array, dt);
+    }
+
+} // namespace nav2_dynamic_util
